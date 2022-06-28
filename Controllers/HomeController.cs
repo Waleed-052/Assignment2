5e772df28a371f53c8de5fa6ec6ae7bd5459b0a4
Admin <Admin@WALEED>
2022-06-28 21:23:21 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
